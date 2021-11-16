762f82ed9c8540acade132bc4400c166311c1c03
Mauricio Jost <mauriciojost@gmail.com>
2021-11-16 12:50:33 +0100

    Add commit information on description

--- merge-main4ino-description.py
+++ merge-main4ino-description.py
@@ -1,10 +1,12 @@
 import json
 import sys
 merged = []
-for f in sys.argv[1:]:
+versionDescription = sys.argv[1]
+jsonFiles = sys.argv[2:]
+for f in jsonFiles:
     with open(f) as fi:
         j = json.load(fi)
         for jj in j['json']:
             merged.append(jj)
-output={"json": merged}
+output={"versionDescription": sys.argv[1], "json": merged}
 print(json.dumps(output))
